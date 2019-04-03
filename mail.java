0565dd697f3b7b2bb0883514d5295fd2d1621f1e
nguyenvanthuyen <44283716+nguyenvanthuyen@users.noreply.github.com>
2019-04-03 09:56:48 +0700

    Add files via upload

---
+++ mail.java
@@ -0,0 +1,10 @@
+package baithi;
+
+public class mail {
+    public static void mail(String [] args) {
+        Hotel hotel1 = new Hotel();
+        hotel1.inputInfo();
+        hotel1.printInfo();
+        Hotel hotel2 = new Hotel();
+    }
+}
